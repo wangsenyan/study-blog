9fc8d4b03246dac65e5b53d3f0d1265bd4657fe4
wangsenyan <2633600702@qq.com>
2021-01-07 01:03:41 +0800

    'zhengli'

--- codes/a.cpp
+++ codes/a.cpp
@@ -1,25 +1,38 @@
 #include<iostream>
 #include<string>
-#define page 4096
+#include<vector>
 
+
+
+using namespace  std;
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        vector<int> bg=new vector<int>(1,1);
+        for(int i=0;i<numRows;i++){
+          vector<int> irow;
+          irow.push(1);
+          for(int j=1;j<i-1;j++){
+            irow.push(bg[i]+bg[i-1]);
+          }
+          irow.push(1);
+          bg=irow;
+          res.push(irow);
+        }
+        return res;
+    }
+};
 int main(int argv,char *argc[])
-{   
-    int a=2,b;
-	b=2+(a+=a++,a+5,++a);
-	std::cout<<b<<std::endl;
-	if(argv<2)
-	{ 
-	   std::cout<<"required argument!";
-	   exit(0);
-	}
-	long long address=atoll(argc[1]);
-	if(address>4294967295)
-	{
-		std::cout<<"invalid address!";
-		exit(0);
-	}
-	std::cout<<"The address "<< address<<" contains:"<<std::endl;
-	std::cout<<"page number = "<< address/page<<std::endl;
-	std::cout<<"offset = "<< address%page<<std::endl;
+{       int num = 10;
+        Solution s = new Solution;
+            int numRows = 2;
+            vector<vector<int>> r =  s.generate(numRows);
+            for(int i=0;i<numRows;i++){
+                for(int j=0;j<i;j++){
+                    cout<<r[i][j];
+                }
+                cout<<endl;
+            }
 	return 0;
 }
